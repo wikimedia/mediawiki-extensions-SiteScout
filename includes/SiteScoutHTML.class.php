db877e786855b55fba7eb08ec7665ee87ef97a71
SamanthaNguyen <samanthanguyen1116@gmail.com>
2017-07-14 16:27:12 -0500

    Reorganize directory structure + file renaming
    Also split SiteScoutClass.php into:
    - includes/SiteScout.class.php
    - includes/SiteScoutHTML.class.php
    - includes/SiteScoutXML.class.php
    
    Bug: T160849
    Change-Id: Idd058e9a66f0cc43d635b6a781db6832b2d4f7c4

---
+++ includes/SiteScoutHTML.class.php
@@ -0,0 +1,71 @@
+<?php
+class SiteScoutHTML extends SiteScout {
+
+	function displayItems() {
+		global $wgExtensionAssetsPath, $wgUserBoard;
+
+		$output = '';
+		$imgPath = $wgExtensionAssetsPath . '/SocialProfile/images/';
+		$this->populateItems();
+		$x = 1;
+
+		foreach ( $this->items as $item ) {
+			if ( $x <= 30 ) {
+				$title = Title::makeTitle( $item['namespace'], $item['pagetitle'] );
+				$user_title = Title::makeTitle( NS_USER, $item['username'] );
+				$output .= '<div id="comment-' . $x . '" class="site-scout"><span class="item-info">';
+				$output .= '<img src="' . $imgPath . $this->getTypeIcon( $item['type'] ) . '" alt="" />';
+
+				if ( $item['minor'] == 1 ) {
+					$output .= '<br /><span class="edit-minor">' . wfMessage( 'sitescout-minor' )->plain() . '</span>';
+				}
+				if ( $item['new'] == 1 ) {
+					$output .= '<br /><span class="edit-new">' . wfMessage( 'sitescout-new' )->plain() . '</span>';
+				}
+				$output .= '</span>';
+
+				if ( $item['type'] != 'networkupdate' ) {
+					$output .= Linker::link(
+						$title,
+						$title->getPrefixedText(),
+						array( 'class' => 'item-title' )
+					);
+				} else {
+					if ( $item['team_id'] ) {
+						$team = SportsTeams::getTeam( $item['team_id'] );
+						$network_name = $team['name'];
+					} else {
+						$sport = SportsTeams::getSport( $item['sport_id'] );
+						$network_name = $sport['name'];
+					}
+					$output .= '<span class="site-scout-network"><a href="' .
+						SportsTeams::getNetworkURL( $item['sport_id'], $item['team_id'] ) .
+						'" class="item-title">' . $network_name . '</a></span>';
+				}
+
+				// $output .= '<span class="item-time">' . date( 'g:i a, m.d.y', $item['timestamp'] ) . '</span>';
+				$comment = $item['comment'];
+				if ( $item['type'] == 'comment' ) {
+					$comment = '<a href="' .
+						htmlspecialchars( $title->getFullURL(), ENT_QUOTES ) .
+						'#comment-' . $item['id'] . '" title="' .
+						htmlspecialchars( $title->getText(), ENT_QUOTES ) . '">' .
+						htmlspecialchars( $item['comment'], ENT_QUOTES ) . '</a>';
+				}
+				$output .= '<span class="item-comment">' . $comment . '</span>';
+
+				$avatar = new wAvatar( $item['userid'], 's' );
+				$commentIcon = $avatar->getAvatarURL();
+				$talk_page = htmlspecialchars( $user_title->getTalkPage()->getFullURL(), ENT_QUOTES );
+				if ( $wgUserBoard ) {
+					$talk_page = UserBoard::getUserBoardURL( $item['username'] );
+				}
+				$output .= '<span class="item-user"><a href="' . htmlspecialchars( $user_title->getFullURL(), ENT_QUOTES ) . '" class="item-user-link">' . $commentIcon . ' ' . $item['username'] . '</a><a href="' . $talk_page . '" class="item-user-talk"><img src="' . $wgExtensionAssetsPath . '/SiteScout/resources/images/talkPageIcon.png" alt="" /></a></span>';
+				$output .= '</div>';
+				$x++;
+			}
+		}
+
+		return $output;
+	}
+}
